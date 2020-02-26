de8447fe1fe231b2e1463ac409bbd3caa4a2ce66
Xavier Perez <xperez007@gmail.com>
2020-02-26 15:23:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,17 +3,18 @@ class Jose
   "Jose", hair_color: "Brown"}
   
   Jose = Person.new(Jose_attributes)
-  Jose.name
-  Jose.hair_color => Brown
-  Jose.eye_color => Brown
-  Jose.height => Medium
-  Jose.weight => Heavy
-  Jose.handed => Right
-  Jose.complexion => Light
-  Jose.t-shirt_size => Medium
-  Jose.wrist_size => Small
-  Jose.glove_size => Large
-  Jose.pant_length => 
+  Jose.name => "Jose"
+  Jose.hair_color => "Brown"
+  Jose.eye_color => "Brown"
+  Jose.height => "Medium"
+  Jose.weight => "Heavy"
+  Jose.handed => "Right"
+  Jose.complexion => "Light"
+  Jose.t-shirt_size => "Medium"
+  Jose.wrist_size => "Small"
+  Jose.glove_size => "Large"
+  Jose.pant_length => "32"
+  
   
   
 end
