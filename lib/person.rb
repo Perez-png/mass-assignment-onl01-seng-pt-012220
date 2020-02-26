6a821bf77663640a2fb05da865f4b7be0ab67f7c
Xavier Perez <xperez007@gmail.com>
2020-02-26 15:04:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,3 @@
-class Person
-  let(:avi) { Person.new(avi_attributes) 
-    :name, :birthday,
-    :hair_color, :eye_color,
-    :height,
-    :weight, :handed,
-    :complexion, :t_shirt_size,
-    :wrist_size, :pant_width
-  end
+class Jose
   
-    
-    
-    
-  }
 end
